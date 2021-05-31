f8bc650caf331c1476e23c243f9374883f746293
wilberquito <wbq.software@gmail.com>
2021-05-31 18:16:32 +0200

    desplazamiento vertical

--- Laser defender/Assets/Scripts/PlayerScript.cs
+++ Laser defender/Assets/Scripts/PlayerScript.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        var deltaX = Input.GetAxis("Horizontal");
-        transform.position = new Vector2(transform.position.x+deltaX, transform.position.y);
+        MoveEngine();
+    }
+
+    void MoveEngine()
+    {
+        // to make delta x independent from velocity of frames per second
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        // to make delta x independent from velocity of frames per second
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.position = new Vector2(transform.position.x + deltaX, transform.position.y + deltaY);
     }
 }
