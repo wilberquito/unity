97d0b4f59da1a8575bdaa7536eceec853835b250
wilberquito <wbq.software@gmail.com>
2021-05-31 14:09:45 +0200

    cambios

--- BLOCK-BRAKER/Assets/Scripts/BallComponent.cs
+++ BLOCK-BRAKER/Assets/Scripts/BallComponent.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BallComponent : MonoBehaviour
@@ -57,24 +58,86 @@ public class BallComponent : MonoBehaviour
         transform.position = paddlePos + startDistanceToPaddle;
     }
 
+    private void HandleBlockCollision()
+    {
+        List<int> randoms = new List<int>();
+
+        float factor = Random.Range(0f, 0.2f);
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            randoms.Add(Random.Range(0, 2));
+        }
+
+        for (int i = 0; i < randoms.Count; i++)
+        {
+            switch (i)
+            {
+                case 0: //
+                    if (randoms[i] == 0)
+                    {
+                        Vector2 v = new Vector2(factor * 1f, 0f);
+                        rigidBody.velocity += v;
+                    }
+                    else
+                    {
+                        Vector2 v = new Vector2(factor * -1f, 0f);
+                        rigidBody.velocity += v;
+                    }
+                    break;
+                case 1:
+                    if (randoms[i] == 0)
+                    {
+                        Vector2 v = new Vector2(0f, factor * 0.5f);
+                        rigidBody.velocity += v;
+                    }
+                    else
+                    {
+                        Vector2 v = new Vector2(0f, factor * -0.5f);
+                        rigidBody.velocity += v;
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void HandlePaddleCollision()
+    {
+
+        float factor = Random.Range(0f, 0.8f);
+        Vector2 vy = new Vector2(0, 1 * factor);
+        rigidBody.velocity += vy;
+
+
+        if (Random.Range(0, 2) == 1)
+        {
+            Vector2 vx = new Vector2(1 * (factor / 2),0);
+            rigidBody.velocity += vx;
+        }
+        else
+        {
+            Vector2 vx = new Vector2(-1 * (factor / 2), 0);
+            rigidBody.velocity += vx;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (ballThrown)
         {
             AudioClip clip = ballAudios[Random.Range(0, ballAudios.Length)];
             audioSource.PlayOneShot(clip);
 
-            float pvx = rigidBody.velocity.x;
-            float pvy = rigidBody.velocity.y;
-            if (Random.Range(0, 2) == 0)
+            // paddle collision
+            if (collision.gameObject == paddle.gameObject)
             {
-                Vector2 vx = new Vector2(1f, 1f);
-                rigidBody.velocity += vx;
+                HandlePaddleCollision();
             }
-            else
+            else // block collision
             {
-                Vector2 vx = new Vector2(-1f, 1f);
-                rigidBody.velocity += vx;
+                //HandleBlockCollision();
             }
         }
     }
